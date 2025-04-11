591870a4b9d10cb31a1c48dc80169b74cbba8a64
Sandali Disara <sandalidissanayake123@gmail.com>
2025-04-11 21:16:01 +0530

    add images section of hero

--- components/Herosection.js
+++ components/Herosection.js
@@ -11,6 +11,27 @@ export default function HeroSection() {
           A Sri Lankan artist who blurs the lines between reality and dreamscapes, weaving emotion, and surrealism into every brushstroke.
         </p>
       </div>
+
+      <div className={styles.artLayout}>
+        <div className={styles.artItem1}>
+          <img src="/images/avatar.png" alt="Art 1" />
+          <p>transforming canvas into portals of introspection and wonder</p>
+        </div>
+
+        <div className={styles.artItem2}>
+          <img src="/images/butters.png" alt="Art 2" />
+          <p>
+            invites viewers to pause, reflect, and lose themselves in stories told through color
+          </p>
+        </div>
+
+        <div className={styles.artItem3}>
+          <img src="/images/angel.png" alt="Art 3" />
+          <p>
+            With each piece, they craft a visual language that is both deeply personal and universally resonant.
+          </p>
+        </div>
+      </div>
     </section>
   );
 }
