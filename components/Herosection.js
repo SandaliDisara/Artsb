c60e044902aba11a66f1660c3d7d8957a8f2ab1e
Sandali Disara <sandalidissanayake123@gmail.com>
2025-04-11 20:18:20 +0530

    create navbar and the top section of Hero

--- components/Herosection.js
+++ components/Herosection.js
@@ -3,8 +3,10 @@ import styles from '@/styles/hero.module.css';
 export default function HeroSection() {
   return (
     <section className={styles.hero}>
-      <div className={styles.titleSection}>
+      <div className={styles.logoBlock}>
         <h1 className={styles.logo}>ARTSB</h1>
+      </div>
+      <div className={styles.textBlock}>
         <p className={styles.description}>
           A Sri Lankan artist who blurs the lines between reality and dreamscapes, weaving emotion, and surrealism into every brushstroke.
         </p>
