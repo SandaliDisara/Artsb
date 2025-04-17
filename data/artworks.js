a8c5faa7ebf835774e4ea8863ef901408fd1071e
Sandali Disara <sandalidissanayake123@gmail.com>
2025-04-17 09:50:08 +0530

    Add arts details

--- data/artworks.js
+++ data/artworks.js
@@ -1,25 +1,37 @@
 export const artworks = [
     {
       id: 'the-night-she',
-      title: 'Angel of the Demons',
+      title: 'The Night She',
       image: '/images/avatar.png',
-      medium: 'Medium Canvas',
-      size: 'Medium Canvas',
-      completed: '05 - 04 - 2025',
+      medium: 'Acrylic',
+      size: '60*30cm canvas',
+      completed: 'Feb 2024',
       status: 'Available for Purchase',
       description:
         'With each piece, they craft a visual language that is both deeply personal and universally resonant, invites viewers to pause, reflect, and lose themselves in stories told through color.',
     },
     {
-      id: 'frog-meditation',
-      title: 'Frog Meditation',
-      image: '/images/frog.png',
-      medium: 'Acrylic on Canvas',
-      size: 'Large Canvas',
-      completed: '12 - 07 - 2024',
+      id: 'day-dream',
+      title: 'Day Dream',
+      image: '/images/butters.png',
+      medium: 'Acrylic',
+      size: '60*90cm canvas',
+      completed: 'Dec 2024',
       status: 'Sold',
       description:
         'A playful yet reflective take on mindfulness. The frog symbolizes inner peace amidst chaos.',
+    },
+
+    {
+      id: 'iam-free',
+      title: 'I’m free',
+      image: '/images/angel.png',
+      medium: 'Acrylic',
+      size: '60*90cm canvas',
+      completed: 'Jan 2024',
+      status: 'Available for Purchase',
+      description:
+        'A dreamy fusion of identity, growth, and freedom—this piece blends wings, florals, and gold to explore the beauty of becoming. Bimasha’s signature use of symbolism and surreal layering invites viewers into a world where emotion meets transformation',
     },
     
   ];
